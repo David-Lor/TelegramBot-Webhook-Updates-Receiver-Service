c88d332fe6f3fe00d8d857d935cbaf3aa3a90fa9
David-Lor <17401854+David-Lor@users.noreply.github.com>
2022-02-21 23:27:56 +0100

    chore: log setup webhook URL

--- telegrambot_receiver_service/services/telegram.py
+++ telegrambot_receiver_service/services/telegram.py
@@ -19,7 +19,7 @@ async def _request(endpoint: str, body: Optional[dict]):
 
 
 async def setup_webhook(url: str):
-    logger.info("Asking Telegram to setup webhook...")
+    logger.info(f"Asking Telegram to setup webhook (url={url})...")
     data = dict(url=url, max_connections=100)
     await _request(endpoint="setWebhook", body=data)
     logger.info("Telegram webhook set")
