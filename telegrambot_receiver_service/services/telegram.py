4ce43c4249843db4682fd6676f1b7ea2dcd59216
David-Lor <17401854+David-Lor@users.noreply.github.com>
2022-02-21 23:33:23 +0100

    chore: log Bot API response before raising for status

--- telegrambot_receiver_service/services/telegram.py
+++ telegrambot_receiver_service/services/telegram.py
@@ -13,8 +13,8 @@ async def _request(endpoint: str, body: Optional[dict]):
         url = urllib.parse.urljoin(telegram_settings.api_url, f"/bot{telegram_settings.token}/{endpoint}")
 
         response = await client.post(url, json=body)
-        response.raise_for_status()
         logger.debug(f"Telegram bot API call to {endpoint} returned {response.status_code} {response.text}")
+        response.raise_for_status()
         return response
 
 
